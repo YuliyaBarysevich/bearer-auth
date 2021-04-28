85ba60b88d2ee613cf97d6387446178c65591ea2
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-04-27 20:12:08 -0700

    all tests passed

---
+++ src/auth/models/users.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+const users = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, requited: true }
+}, { toJSON: { virtuals: true }});
+
+users.virtual('token').get(function () {
+  let tokenObject = {
+    username: this.username
+  }
+  return jwt.sign(tokenObject, process.env.SECRET)
+});
+
+users.pre('save', async function() {
+  if (this.isModified('password')){
+    this.password = await bcrypt.hash(this.password, 10)
+  }
+});
+
+users.statics.authenticateBasic = async function (username, password) {
+  const user = await this.findOne({ username });
+  const valid = await bcrypt.compare(password, user.password);
+  if(valid) {return user}
+  throw new Error ('Invalid User')
+}
+
+users.statics.authenticateWithToken = async function (token) {
+  try {
+    const parsedToken = jwt.verify(token, process.env.SECRET)
+    const user = await this.findOne({ username: parsedToken.username })
+    if(user) { return user }
+    throw new Error('User Not Found');
+  }catch(e) {
+    throw new Error (e.message)
+  }
+}
+
+module.exports = mongoose.model('users', users)
